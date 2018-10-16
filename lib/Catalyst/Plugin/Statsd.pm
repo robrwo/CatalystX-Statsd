95b22bbb4d16f66093247d4b2206786f9ab30c75
Robert Rothenberg <rrwo@cpan.org>
2018-10-16 21:58:11 +0100

    Perltidy

--- lib/Catalyst/Plugin/Statsd.pm
+++ lib/Catalyst/Plugin/Statsd.pm
@@ -9,10 +9,9 @@ use POSIX qw/ ceil /;
 use namespace::autoclean;
 
 around finalize => sub {
-    my ($next, $c) = @_;
+    my ( $next, $c ) = @_;
 
-    if ( my $client = $c->req->env->{'psgix.monitor.statsd'} )
-{
+    if ( my $client = $c->req->env->{'psgix.monitor.statsd'} ) {
         if ( $c->use_stats ) {
 
             my $elapsed = $c->stats->elapsed;
@@ -20,14 +19,14 @@ around finalize => sub {
             $client->timing_ms( "catalyst.response.time",
                 ceil( $elapsed * 1000 ) );
 
-            foreach my $stat ($c->stats->report) {
+            foreach my $stat ( $c->stats->report ) {
 
                 my $metric = "catalyst.stats." . $stat->[1] . ".time";
                 my $timing = ceil( $stat->[2] * 1000 );
 
                 $metric =~ s/\W+/./g;
 
-                $client->timing_ms($metric, $timing);
+                $client->timing_ms( $metric, $timing );
 
             }
 
