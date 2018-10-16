a9524aee39d164dae353726bf85d6ff4f77f101a
Robert Rothenberg <rrwo@cpan.org>
2018-10-16 16:20:45 +0100

    Use smarter method for escaping non-word characters
    This also allows the code to work with older Perl versions.

--- lib/CatalystX/Statsd.pm
+++ lib/CatalystX/Statsd.pm
@@ -11,22 +11,25 @@ use namespace::autoclean;
 around finalize => sub {
     my ($next, $c) = @_;
 
-    if ( my $client = $c->req->env->{'psgix.monitor.statsd'} ) {
-
+    if ( my $client = $c->req->env->{'psgix.monitor.statsd'} )
+{
         if ( $c->use_stats ) {
 
             my $elapsed = $c->stats->elapsed;
 
-            $client->timing( "catalyst.response.time",
+            $client->timing_ms( "catalyst.response.time",
                 ceil( $elapsed * 1000 ) );
 
-            my @report =
-              map { [ $_->[1] =~ s/\W+/./gr, ceil( $_->[2] * 1000 ), ], }
-              $c->stats->report;
+            foreach my $stat ($c->stats->report) {
 
-            $client->timing( sprintf( 'catalyst.stats.%s.time', $_->[0] ),
-                $_->[1] )
-              for @report;
+                my $metric = "catalyst.stats." . $stat->[1] . ".time";
+                my $timing = ceil( $stat->[2] * 1000 );
+
+                $metric =~ s/\W+/./g;
+
+                $client->timing_ms($metric, $timing);
+
+            }
 
         }
 
